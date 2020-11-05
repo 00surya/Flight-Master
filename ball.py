4297dff046246a61141a8d8d3de5d3dd7239f71e
snehill <issnh@gmail.com>
2020-11-06 01:10:23 +1400

    all files commited

--- ball.py
+++ ball.py
@@ -2,7 +2,8 @@ import pygame
 import random
 
 player='gallery\sprites\player.png'
-background='gallery\sprites\\background.png'
+background='gallery\sprites\\background.jpg'
+dino='gallery\sprites\dino.png'
 # pygame.init()
 
 screen = pygame.display.set_mode((400, 480))
@@ -12,7 +13,8 @@ velocity = [0, 0]
 speed = 300  
 dx = []  
 xx=-2
-ball_no=0
+back_Y=-700
+ball_no=-9
 def welcome_screen():
     image=pygame.image.load(player)
     rectt = image.get_rect()
@@ -37,11 +39,13 @@ def main_game():
     rectt = imagee.get_rect()
     rectt.center =(200,300)
     
-    
     yyy=20
-    ballx=11
+    ballx=random.randrange(12,350)
+    px=-12
     while True:
+        # print(cv)
         global xx
+        global back_Y
         dt = clock.tick(FPS) / 1000.0  # This will give me the time in seconds between each loop.
         
         
@@ -58,26 +62,66 @@ def main_game():
                     dx.insert(0, -speed)
                 elif event.key == pygame.K_RIGHT:
                     dx.insert(0, speed)
-            
+
+        
+        
         if dx:  
-            rectt.x += dx[0] * dt
+            rectt.x+=dx[0] * dt
+            if rectt.x>345 or rectt.x<-3:
+                lost()
+            # print(rectt.x)
+
+        # What happend if plane were crashed?    
+        # if ballx+image.get_width()+4>rectt.x-10>ballx+image.get_width()-4:
+
+        # ball sqare area ballx
+
+        # if ballx==rectt.x or (rectt.x)+1>ballx>rectt.x:    
+            # print(ballx)
+        #     print(rectt.x)
+
+        #     # print("hello this is snehiljdhwu cuhdu ydusvy scdgybxerd7 wbel")
+        #     lost()
+
         f = getupdate()
+        
+        bck_motion=bck_update()
         screen.fill((0, 0, 0))
-        bally=yyy+f
+        bally=yyy+f   
         if bally> 500:
             xx=-2
             yyy=20
-            ballx=random.randrange(11,300)
+            ballx=random.randrange(11,350)
+        
+                            
+            # print(ballx)
+        # pk=bck_motion-700   
         screen.fill((0, 0, 0))
-        screen.blit(bck_image, (0,0))
+        screen.blit(bck_image, (0,bck_motion))
+        screen.blit(bck_image, (0,(bck_motion-1300)))
+        if bck_motion > 860:
+            back_Y=-700
+            
         screen.blit(imagee, rectt)
         screen.blit(image,(ballx,bally))
 
         pygame.display.update()
+def lost():
+    radfefr
+    # lost=pygame.image.load(dino).convert_alpha()
+    # dinoo = lost.get_rect()
+    # dinoo.center =(20,30)
+    # screen.blit(lost,dinoo)
+        
+      
 def getupdate():
     global xx
     xx+=15
     return xx
+def bck_update():    
+    global back_Y
+    back_Y+=1
+    return back_Y
 ball_no=0    
 def get_ball():
     global ball_no
@@ -92,8 +136,7 @@ def get_ball():
     if xx==-2:
         ball_no=random.randrange(0,6)    
     d=Balls[ball_no]
-    return str(d)
-    
+    return str(d)    
 if __name__ == "__main__":
     pygame.init()    
     clock = pygame.time.Clock()
--- flight_master.py
+++ flight_master.py
@@ -1,89 +1,40 @@
 import pygame
-import os
-import random
-import pyautogui
-player='gallery\sprites\player.png' #
-background='gallery\sprites\\background.png' #
+pygame.init()
+
 screen = pygame.display.set_mode((720, 480))
+clock = pygame.time.Clock()
 FPS = 30
-GAME_SPRITES={}
-pip=True
-def welcome_screen(): #
-    image=pygame.image.load(player)
-    rect = image.get_rect()
-    rect.center =(200,300)
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pip=False
-                raise SystemExit
+x=350
+rect = pygame.Rect((350, 220), (32, 32))  # Often used to track the position of an object in pygame.
+image = pygame.Surface((32, 32))  # Images are Surfaces, so here I create an 'image' from scratch since I don't have your image.
+image.fill(pygame.Color('white'))  # I fill the image with a white color.
+velocity = [0, 0]  # This is the current velocity.
+speed = 200  # This is the speed the player will move in (pixels per second).
+dx = []  # This will be our queue. It'll keep track of the horizontal movement.
 
-            elif event.type == pygame.KEYDOWN and (event.key == pygame.K_SPACE):
-                return
-            else:
-                screen.fill((0, 0, 0))
-                screen.blit(bck_image, (0,0))
-                screen.blit(image, rect)
-                pygame.display.update()
+while True:
+    dt = clock.tick(FPS) / 1000.0  # This will give me the time in seconds between each loop.
 
-def main_game():
-    image=pygame.image.load(player)#
-    rect = image.get_rect()#
-    rect.center =(200,300)#
-    # rect = pygame.Rect((350, 220), (32, 32))  # Often used to track the position of an object in pygame.
-    # image = pygame.Surface((32, 32))  # Images are Surfaces, so here I create an 'image' from scratch since I don't have your image.
-    # image.fill(pygame.Color('white'))  # I fill the image with a white color.
-    # ballVellY=-10
-    # ballxy={'x':random.randrange(10,15),'y':-5}
-    # velocity = [0, 0]  # This is the current velocity.
-    # speed = 200  # This is the speed the player will move in (pixels per second).
-    # dx = []  # This will be our queue. It'll keep track of the horizontal movement.
-    while True:
-        
-        dt = clock.tick(FPS) / 1000.0  # This will give me the time in seconds between each loop.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            raise SystemExit
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                dx.insert(0, -speed)
+            elif event.key == pygame.K_RIGHT:
+                dx.insert(0, speed)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                dx.remove(-speed)
+            elif event.key == pygame.K_RIGHT:
+                dx.remove(speed)
 
-        for event in pygame.event.get():#
-            if event.type == pygame.QUIT:#
-                raise SystemExit#
-            elif event.type == pygame.KEYDOWN:#
-                if event.key == pygame.K_LEFT:#
-                    dx.insert(0, -speed)#
-                elif event.key == pygame.K_RIGHT:#
-                    dx.insert(0, speed) #
-            # elif event.type == pygame.KEYUP:
-                # if event.key == pygame.K_LEFT:
-                    # dx.remove(-speed)
-                # elif event.key == pygame.K_RIGHT:
-                    # dx.remove(speed)
+    if dx:  # If there are elements in the list.
+        rect.x += dx[0] * dt
 
-        if dx:  # If there are elements in the list.
-            rect.x += dx[0] * dt
+    screen.fill((0, 0, 0))
+    screen.blit(image, rect)
+    pygame.display.update()
 
-
-        # balls motion in y axis
-        # p=balls()
-        # current_ball=p[random.randrange(0,4)]
-        
-
-        # Add a new ball when the first is about to cross the leftmost part of the screen
-        # if 0<bally_x<5:
-        #     new_ballx,new_bally=random.randrange(100,150),-5 
-        #     ballx.append(new_ballx)
-        #     bally.append(new_bally)    
-
-        # if the ball is out of the screen, remove it
-        # if ballx['x'] < - GAME_SPRITES['pipe'][0].get_width():
-            # upperPipes.pop(0)
-            # lowerPipes.pop(0)        
-        
-
-
-        screen.fill((0, 0, 0))
-        screen.blit(bck_image, (0,0))
-        screen.blit(image, rect)
-        pygame.display.update()
-
-        # print dx  # Uncomment to see what's happening.
-
-        
-
+    print(dx)  # Uncomment to see what's happening.
+    print(x)
